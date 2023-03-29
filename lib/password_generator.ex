6be56b8993c25f430c77cce61a6613150fb6c633
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2023-03-29 23:51:32 +0700

    add module

--- lib/password_generator.ex
+++ lib/password_generator.ex
@@ -1,18 +1,75 @@
 defmodule PasswordGenerator do
   @moduledoc """
-  Documentation for `PasswordGenerator`.
+  Generates random password depending on parameters, Module main function is `generate(options)`.
+  That function takes the options map.
+  Options example:
+    options = %{
+      "length" => "5",
+      "numbers" => "false",
+      "uppercase" => "false",
+      "symbols" => "false",
+    }
+  The options are only 4, `length`, `numbers`, `uppercase`, and `symbols`.
   """
+
+  @allowed_options [:length, :numbers, :uppercase, :symbols]
 
   @doc """
-  Hello world.
+  Generates password for given options:
 
   ## Examples
+    options = %{
+      "length" => "5",
+      "numbers" => "false",
+      "uppercase" => "false",
+      "symbols" => "false",
+    }
 
-      iex> PasswordGenerator.hello()
-      :world
+    iex> PasswordGenerator.generate(options)
+    "abcdf"
 
+    options = %{
+      "length" => "5",
+      "numbers" => "true",
+      "uppercase" => "false",
+      "symbols" => "false",
+    }
+
+     iex> PasswordGenerator.generate(options)
+    "abcd1"
   """
-  def hello do
-    :world
+  @spec generate(options :: map()) :: {:ok, bitstring()} | {:error, bitstring()}
+  def generate(options) do
+    length = Map.has_key?(options, "length")
+
+    validate_length(length, options)
+  end
+
+  defp validate_length(false, _options), do: {:error, "Please provide a length"}
+
+  defp validate_length(true, options) do
+    numbers = Enum.map(0..9, & Integer.to_string(&1))
+    length = options["length"]
+    length = String.contains?(length, numbers)
+    validate_length_is_integer(length, options)
+  end
+
+  defp validate_length_is_integer(false, _options), do: {:error, "Only integer allowed for length"}
+
+  defp validate_length_is_integer(true, options) do
+    length = options["length"] |> String.trim() |> String.to_integer()
+    options_without_length = Map.delete(options, "length")
+    options_values = Map.values(options_without_length)
+
+    value =
+      options_values
+      |> Enum.all?(fn x -> String.to_atom(x) |> is_boolean() end)
+
+    validate_options_values_are_boolean(value, options)
+  end
+
+  defp validate_options_values_are_boolean(false, _options), do: {:error, "Rest options value only allowed boolean type"}
+
+
   end
 end
--- test/password_generator_test.exs
+++ test/password_generator_test.exs
@@ -2,7 +2,83 @@ defmodule PasswordGeneratorTest do
   use ExUnit.Case
   doctest PasswordGenerator
 
-  test "greets the world" do
-    assert PasswordGenerator.hello() == :world
+  setup do
+    options = %{
+      "length" => "10",
+      "numbers" => "false",
+      "uppercase" => "false",
+      "symbols" => "false"
+    }
+
+    options_type = %{
+      lowercase: Enum.map(?a..?z, & <<&1>>),
+      numbers: Enum.map(0..9, & Integer.to_string(&1)),
+      uppercase: Enum.map(?A..?Z, & <<&1>>),
+      symbols: String.split("!@#$%^&*()_-+{}|:<=>?`~", "", trim: true)
+    }
+
+    {:ok, result} = PasswordGenerator.generate(options)
+
+    %{
+      options_type: options_type,
+      result: result
+    }
+  end
+
+  test "returns a string", %{result: result} do
+    assert is_bitstring(result)
+  end
+
+  test "returns error when no length is given" do
+    options = %{"invalid" => "false"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "returns error when length is not an integer" do
+    options = %{"length" => "ab"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "length of returned string is option provided" do
+    length_option = %{"length" => "5"}
+    {:ok, result} = PasswordGenerator.generate(length_option)
+
+    assert 5 = String.length(result)
+  end
+
+  test "returns a lowercase string just with the length", %{options_type: options} do
+    length_option = %{"length" => "5"}
+    {:ok, result} = PasswordGenerator.generate(length_option)
+
+    assert String.contains?(result, options.lowercase)
+
+    refute String.contains?(result, options.numbers)
+    refute String.contains?(result, options.uppercase)
+    refute String.contains?(result, options.symbols)
+  end
+
+  test "returns error when options values are not booleans" do
+    options = %{
+      "length" => "10",
+      "number" => "invalid",
+      "uppercase" => "0",
+      "symbols" => "false"
+    }
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "returns error when options not allowed" do
+    options = %{"length" => "5", "invalid" => "true"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "returns error when one option not allowed" do
+    options = %{"length" => "5", "numbers" => "true", "invalid" => "true"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
   end
 end
